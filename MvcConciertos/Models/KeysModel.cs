30f0e0957f36810b0128d2c00992e2f0714ae3c2
SergioGuijarrodeCabo <sguijarrodcabo@gmail.com>
2023-06-08 11:29:29 +0200

    v1

---
+++ MvcConciertos/Models/KeysModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MvcConciertos.Models
+{
+     public class KeysModel {
+        public string ApiUrl { set; get; }
+    }
+}
---
+++ MvcConciertos/Services/ServiceConciertos.cs
@@ -0,0 +1,91 @@
+﻿using MvcConciertos.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MvcConciertos.Services
+{
+
+
+    public class ServiceConciertos
+    {
+
+        private MediaTypeWithQualityHeaderValue header;
+        private string UrlApi;
+
+        public ServiceConciertos(IConfiguration configuration, KeysModel keysModel)
+        {
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+            this.UrlApi = keysModel.ApiUrl;
+        }
+
+        #region GENERAL
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                string url = this.UrlApi + request;
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+        #endregion 
+
+        #region CRUD
+        
+
+        public async Task<List<Evento>> GetEventosAsync()
+        {
+            string request = "/api/evento/geteventos";
+            List<Evento> eventos = await this.CallApiAsync<List<Evento>>(request);
+            return eventos;
+        }
+
+        public async Task<List<CategoriaEvento>> GetCategoriaEventosAsync()
+        {
+            string request = "/api/evento/getcategorias";
+            List<CategoriaEvento> categorias = await this.CallApiAsync<List<CategoriaEvento>>(request);
+            return categorias;
+        }
+
+
+        public async Task<List<Evento>> GetEventosporCategoriaAsync(int idcategoria)
+        {
+            string request = "/api/evento/"  + idcategoria;
+            List<Evento> eventos = await this.CallApiAsync<List<Evento>>(request);
+            return eventos;
+        }
+
+
+        public async Task CreateEventoAsync(Evento evento)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/evento";
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+
+                string json = JsonConvert.SerializeObject(evento);
+
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(this.UrlApi + request, content);
+            }
+        }
+
+       
+       
+        #endregion
+    }
+}
